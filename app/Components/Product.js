76d16b2a811a034cd113c7dfb50fb3e6e0084ae7
abdullah_mamun369 <maamamun369@gmail.com>
2023-11-04 10:27:40 +0600

    Individual image selection is done

--- app/Components/Product.js
+++ app/Components/Product.js
@@ -1,21 +1,29 @@
 "use client"
 
 import Image from 'next/image'
+import { useState } from 'react';
 
 const Product = (props) => {
 
-    const { product, isChecked, handleSelected } = props;
+    const { product, handleSelected } = props;
     const { id, imageUrl } = product;
 
-    console.log(imageUrl);
+    const [isChecked, setIsChecked] = useState(false)
 
-    console.log(id);
+    console.log(isChecked);
+
+    // console.log(imageUrl);
+
+    // console.log(id);
     return (
         <div className='group'>
             <div className='border rounded-lg relative overflow-hidden'>
                 <Image className='rounded-lg' src={imageUrl} alt='ollyo_product' width={1500} height={1500}></Image>
                 <div className='absolute h-full w-full bg-black/40 flex justify-end p-5 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-                    <input checked={isChecked} onChange={handleSelected} className='form-checkbox h-6 w-6 rounded-full text-purple-600 bg-purple-600' type="checkbox" />
+                    <input onChange={() => {
+                        setIsChecked(!isChecked);
+                        handleSelected(id, isChecked);
+                    }} className='form-checkbox h-6 w-6 rounded-full text-purple-600 bg-purple-600' type="checkbox" />
                 </div>
             </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -11,54 +11,44 @@ export default function Home() {
 
   const [originalImage, setOriginalImage] = useState([])
   const [selected, setSelected] = useState(0)
-  const [isChecked, setIsChecked] = useState(false)
+  // const [isChecked, setIsChecked] = useState(false)
+  const [shuffledData, setShuffledData] = useState(originalImage);
 
-
-
-
-  const handleSelected = () => {
-    setIsChecked(!isChecked);
+  const handleSelected = (id, isChecked) => {
     setSelected(isChecked ? selected - 1 : selected + 1)
   }
 
 
   useEffect(() => {
 
+    function shuffleArray(array) {
+      const shuffledArray = [...array];
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      return shuffledArray;
+    }
+
+    const shuffle = () => {
+      const newShuffledData = (originalImage.length > 0) && shuffleArray(originalImage);
+      setShuffledData(newShuffledData);
+    };
+
     fetch("images.json")
       .then(res => res.json())
       .then(data => setOriginalImage(data))
-
+      .then(shuffle())
   }, [])
 
 
-  // // const [shuffledData, setShuffledData] = useState(originalImage);
 
 
 
-  // function shuffleArray(array) {
-  //   const shuffledArray = [...array];
-  //   for (let i = shuffledArray.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  //   }
-  //   return shuffledArray;
-  // }
 
-  // const shuffle = () => {
-  //   const newShuffledData = shuffleArray(originalImage);
-  //   setShuffledData(newShuffledData);
-  // };
+  // console.log(shuffledData);
 
 
-  // console.log(originalImage);
-
-  // const selectedImageIndex = originalImage[0];
-
-  // // const selectedImageUrl = selectedImageIndex.imageUrl;
-
-  // // console.log(selectedImageUrl);
-
-  // console.log(originalImage);
 
   return (
     <>
@@ -84,16 +74,15 @@ export default function Home() {
               <div className='border rounded-lg relative overflow-hidden'>
                 <Image className='rounded-lg' src={image1} alt='ollyo_product' width={1500} height={1500}></Image>
                 <div className='absolute h-full w-full bg-black/40 flex justify-end p-5 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-                  <input checked={isChecked} onChange={handleSelected} className='form-checkbox h-6 w-6 rounded-full text-purple-600 bg-purple-600' type="checkbox" />
+                  <input onChange={handleSelected} className='form-checkbox h-6 w-6 rounded-full text-purple-600 bg-purple-600' type="checkbox" />
                 </div>
               </div>
             </div>
           </div>
-          {originalImage &&
-            originalImage.map(product => <Product
+          {shuffledData &&
+            shuffledData.map(product => <Product
               key={product.id}
               product={product}
-              isChecked={isChecked}
               handleSelected={handleSelected}
             >
             </Product>)
