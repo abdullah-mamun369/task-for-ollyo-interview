5eb24176e53f2ce3dc66fe6379a135f08e80d727
abdullah_mamun369 <maamamun369@gmail.com>
2023-11-04 12:15:38 +0600

    Checkbox are shown when it is checked

--- app/page.js
+++ app/page.js
@@ -9,10 +9,9 @@ import Product from './Components/Product'
 
 export default function Home() {
 
-  const [originalImage, setOriginalImage] = useState([])
   const [selected, setSelected] = useState(0)
   // const [isChecked, setIsChecked] = useState(false)
-  const [shuffledData, setShuffledData] = useState(originalImage);
+  const [shuffledData, setShuffledData] = useState([]);
 
   const handleSelected = (id, isChecked) => {
     setSelected(isChecked ? selected - 1 : selected + 1)
@@ -20,29 +19,32 @@ export default function Home() {
 
 
   useEffect(() => {
-
-    function shuffleArray(array) {
-      const shuffledArray = [...array];
-      for (let i = shuffledArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-      }
-      return shuffledArray;
-    }
-
-    const shuffle = () => {
-      const newShuffledData = (originalImage.length > 0) && shuffleArray(originalImage);
-      setShuffledData(newShuffledData);
-    };
-
     fetch("images.json")
       .then(res => res.json())
-      .then(data => setOriginalImage(data))
-      .then(shuffle())
+      .then(data => shuffle(data))
   }, [])
 
+  //   function shuffleImg(img) {
+  //     for (let i = 0; i < img.length; i++) {
+  //         const j = Math.floor(Math.random() * img.length);
+  //         [img[i], img[j]] = [img[j], img[i]];
+  //     }
+  // }
 
 
+  function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  }
+
+  const shuffle = (data) => {
+    const newShuffledData = (data.length > 0) && shuffleArray(data);
+    setShuffledData(newShuffledData);
+  };
 
 
 
@@ -62,28 +64,23 @@ export default function Home() {
           <div className='h-16 flex items-center mx-auto w-full xl:w-8/12 justify-between'>
             {/* <input className='form-checkbox h-6 w-6 rounded-full text-purple-600' type="checkbox" checked /> */}
             <h3 className='flex items-center text-xl font-bold'><BsFillCheckSquareFill className=' text-purple-700 mr-2' /> {selected} Files Selected</h3>
-            <button className='font-bold text-purple-700 hover:text-red-700'>Delete file</button>
+            <div>
+              <button onClick={() => shuffle(shuffledData)} className='font-bold text-purple-700 hover:text-red-700 mr-5'>Shuffle Images</button>
+              <button className='font-bold text-purple-700 hover:text-red-700'>Delete file</button>
+            </div>
           </div>
         </nav>
 
         {/* Drag&Drop box start================================================= */}
 
         <section className='mx-auto w-full xl:w-8/12 grid grid-cols-5 gap-3'>
-          <div className='col-span-2 row-span-2 border rounded-lg'>
-            <div className='group'>
-              <div className='border rounded-lg relative overflow-hidden'>
-                <Image className='rounded-lg' src={image1} alt='ollyo_product' width={1500} height={1500}></Image>
-                <div className='absolute h-full w-full bg-black/40 flex justify-end p-5 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-                  <input onChange={handleSelected} className='form-checkbox h-6 w-6 rounded-full text-purple-600 bg-purple-600' type="checkbox" />
-                </div>
-              </div>
-            </div>
-          </div>
+
           {shuffledData &&
-            shuffledData.map(product => <Product
+            shuffledData.map((product, index) => <Product
               key={product.id}
               product={product}
               handleSelected={handleSelected}
+              index={index}
             >
             </Product>)
           }
