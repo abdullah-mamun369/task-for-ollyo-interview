338a7ede4cf814cc28016c2c5cefb5ee8f83339d
abdullah_mamun369 <maamamun369@gmail.com>
2023-11-06 18:26:29 +0600

    Add image section has added

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@
 
 import { BsFillCheckSquareFill } from 'react-icons/bs'
 import { TbArrowsShuffle2 } from 'react-icons/tb'
+import { BsCardImage } from 'react-icons/bs'
 import { useEffect, useState } from 'react'
 import Product from './Components/Product'
 import { DragDropContext, Droppable } from "react-beautiful-dnd"
@@ -93,54 +94,55 @@ export default function Home() {
 
   return (
     <>
-      <main>
 
-        {/* Navbar start================================================= */}
-        <nav className='border-b-2 mb-5'>
-          {
-            selected === 0 ?
-              <div className='h-16 flex items-center justify-between mx-auto w-full md:w-10/12 xl:w-8/12 p-5 md:p-0 '>
-                <h1 className='text-3xl font-bold'>Gallery</h1>
-                <button onClick={() => shuffle(shuffledData)} className='font-bold text-purple-700 hover:text-blue-700 mr-5 hidden md:flex items-center'> <TbArrowsShuffle2 className='mr-2 text-2xl' /> Shuffle Images</button>
+      {/* Navbar start================================================= */}
+      <nav className='border-b-2 mb-5'>
+        {
+          selected === 0 ?
+            <div className='h-16 flex items-center justify-between mx-auto w-full md:w-10/12 xl:w-8/12 p-5 md:p-0 '>
+              <h1 className='text-3xl font-bold'>Gallery</h1>
+              <button onClick={() => shuffle(shuffledData)} className='font-bold text-purple-700 hover:text-blue-700 mr-5 hidden md:flex items-center'> <TbArrowsShuffle2 className='mr-2 text-2xl' /> Shuffle Images</button>
+            </div>
+            :
+            <div className='h-16 flex items-center mx-auto w-full md:w-10/12 xl:w-8/12 p-5 md:p-0 justify-between'>
+              {/* <input className='form-checkbox h-6 w-6 rounded-full text-purple-600' type="checkbox" checked /> */}
+              <h3 className='flex items-center text-xl font-bold'><BsFillCheckSquareFill className=' text-purple-700 mr-2' /> {selected} Files Selected</h3>
+              <button onClick={() => shuffle(shuffledData)} className='font-bold text-purple-700 hover:text-blue-700 mr-5 hidden md:flex items-center'> <TbArrowsShuffle2 className='mr-2 text-2xl' /> Shuffle Images</button>
+              <button onClick={handleDelete} className='font-bold text-red-700'>Delete file</button>
+
+            </div>
+        }
+      </nav>
+
+      {/* Drag&Drop box start================================================= */}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div>
+          <Droppable droppableId="imagesss" type="group" direction="horizontal">
+
+            {(provided) => (
+
+              <div {...provided.droppableProps} ref={provided.innerRef} className='mx-auto grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-3 md:w-10/12 xl:w-8/12 p-5 md:p-0'>
+                {shuffledData &&
+                  shuffledData.map((product, index) => <Product
+                    key={product.id}
+                    product={product}
+                    handleSelected={handleSelected}
+                    index={index}
+                    draggableId={product.id}
+                  >
+                  </Product>)
+                }
+                <button className='w-full h-full border-2 border-gray-400 border-dashed rounded-lg bg-gray-100 flex flex-col justify-center items-center'>
+                  <BsCardImage className='text-4xl text-gray-500' />
+                  <h1 className='text-2xl font-bold text-gray-500'>Add Images</h1>
+                </button>
               </div>
-              :
-              <div className='h-16 flex items-center mx-auto w-full md:w-10/12 xl:w-8/12 p-5 md:p-0 justify-between'>
-                {/* <input className='form-checkbox h-6 w-6 rounded-full text-purple-600' type="checkbox" checked /> */}
-                <h3 className='flex items-center text-xl font-bold'><BsFillCheckSquareFill className=' text-purple-700 mr-2' /> {selected} Files Selected</h3>
-                <button onClick={() => shuffle(shuffledData)} className='font-bold text-purple-700 hover:text-blue-700 mr-5 hidden md:flex items-center'> <TbArrowsShuffle2 className='mr-2 text-2xl' /> Shuffle Images</button>
-                <button onClick={handleDelete} className='font-bold text-red-700'>Delete file</button>
 
-              </div>
-          }
-        </nav>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
 
-        {/* Drag&Drop box start================================================= */}
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <div>
-            <Droppable droppableId="imagesss" type="group" direction="horizontal">
-
-              {(provided) => (
-
-                <div {...provided.droppableProps} ref={provided.innerRef} className='mx-auto grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-3 md:w-10/12 xl:w-8/12 p-5 md:p-0'>
-                  {shuffledData &&
-                    shuffledData.map((product, index) => <Product
-                      key={product.id}
-                      product={product}
-                      handleSelected={handleSelected}
-                      index={index}
-                      draggableId={product.id}
-                    >
-                    </Product>)
-                  }
-                </div>
-
-              )}
-            </Droppable>
-          </div>
-        </DragDropContext>
-
-
-      </main>
     </>
   )
 }
